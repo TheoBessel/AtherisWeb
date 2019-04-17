c697fa04bfe237051fc8ef3702d5ebf2727d24ea
TheoBessel <theobessel@fatserver.net>
2019-04-17 18:45:17 +0200

    Using UrlGetter Constructor for global init

--- url/UrlGetter.cpp
+++ url/UrlGetter.cpp
@@ -8,14 +8,14 @@
 #include "../usual/files.hpp"
 
 UrlGetter::UrlGetter() {
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
 }
 
 size_t UrlGetter::debugCallback(char* buf, size_t size, size_t nmemb, void* up) {
     for (int c = 0; c<size*nmemb; c++)
     {
-        //usual::clog(buf);
         static_cast<std::string*>(up)->push_back(buf[c]);
-
     }
     return size*nmemb;
 }
@@ -30,19 +30,21 @@ void UrlGetter::urlFormater(std::string url) {
 std::string UrlGetter::get(std::string url) {
     data = "";
     urlFormater(url);
-    CURL *curl;
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl = curl_easy_init();
+
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &data);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &UrlGetter::debugCallback);
     #ifndef NDEBUG
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
     #endif
+
     curl_easy_perform(curl);
     curl_easy_cleanup(curl);
+
     curl_global_cleanup();
+
     usual::clog("Url get success");
     usual::writeFile(data, "/Users/theobessel/Desktop/test.html");
+
     return data;
 }
--- url/UrlGetter.hpp
+++ url/UrlGetter.hpp
@@ -6,16 +6,18 @@
 #define WEBRENDER_URLGETTER_HPP
 
 #include "../usual/logger.hpp"
+#include <curl/curl.h>
 
 class UrlGetter {
 public:
     UrlGetter();
-    std::string data;
-    static size_t debugCallback(char* buf, size_t size, size_t nmemb, void* up);
     void urlFormater(std::string url);
     std::string get(std::string url);
 
 private:
+    CURL *curl;
+    std::string data;
+    static size_t debugCallback(char* buf, size_t size, size_t nmemb, void* up);
 
 };
 
