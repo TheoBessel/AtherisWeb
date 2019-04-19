a047bc4d7692d259451fd1d17b3cb4d1df364eac
TheoBessel <theobessel@fatserver.net>
2019-04-19 22:13:00 +0200

    Keywords list 3 (lexer)

--- compiler/html/htmlLexer.cpp
+++ compiler/html/htmlLexer.cpp
@@ -186,6 +186,7 @@ enum ATTRIBUTES {
     A_ALIGN,
     A_ALLOW,
     A_ALT,
+    A_ARCHIVE,
     A_ASYNC,
     A_AUTOCAPITALIZE,
     A_AUTOCOMPLETE,
@@ -212,6 +213,8 @@ enum ATTRIBUTES {
     A_CROSSORIGIN,
     A_CSP,
     A_DATA,
+    A_DATAFLD,
+    A_DATASRC,
     A_DATAUNIV,
     A_DATETIME,
     A_DEFAULT,
@@ -232,6 +235,7 @@ enum ATTRIBUTES {
     A_HIGH,
     A_HREF,
     A_HREFLANG,
+    A_HSPACE,
     A_HTTPEQUIV,
     A_ICON,
     A_ID,
@@ -250,6 +254,7 @@ enum ATTRIBUTES {
     A_LOW,
     A_MANIFEST,
     A_MAX,
+    A_MAYSCRIPT,
     A_MAXLENGTH,
     A_MINLENGTH,
     A_MEDIA,
@@ -258,7 +263,9 @@ enum ATTRIBUTES {
     A_MULTIPLE,
     A_MUTED,
     A_NAME,
+    A_NOHREF,
     A_NOVALIDATE,
+    A_OBJECT,
     A_OPEN,
     A_OPTIMUM,
     A_PATTERN,
@@ -300,6 +307,7 @@ enum ATTRIBUTES {
     A_TYPE,
     A_USEMAP,
     A_VALUE,
+    A_VSPACE,
     A_WIDTH,
     A_WRAP
 };
@@ -319,33 +327,33 @@ std::unordered_map<const std::regex, const TAGS, const std::vector<ATTRIBUTES> >
         {{"<!DOCTYPE "},{">"},{TAGS::T_DOCTYPE}},
         {{makeRegex("a")},{TAGS::T_A},{{ATTRIBUTES::A_HREF},{ATTRIBUTES::A_HREFLANG},{ATTRIBUTES::A_PING},{ATTRIBUTES::A_REFERRERPOLICY},{ATTRIBUTES::A_REL},{ATTRIBUTES::A_TARGET},{ATTRIBUTES::A_TYPE},{ATTRIBUTES::A_CHARSET},{ATTRIBUTES::A_COORDS},{ATTRIBUTES::A_NAME},{ATTRIBUTES::A_REV},{ATTRIBUTES::A_SHAPE}}},
         {{makeRegex("abbr")},{TAGS::T_ABBR}},
-        {{makeRegex("acronym")},{TAGS::T_ACRONYM},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("address")},{TAGS::T_ADDRESS},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("applet")},{TAGS::T_APPLET},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeEmptyTagRegex("area")},{TAGS::T_AREA},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// empty
-        {{makeRegex("article")},{TAGS::T_ARTICLE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("aside")},{TAGS::T_ASIDE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("acronym")},{TAGS::T_ACRONYM}}, /// obsolete
+        {{makeRegex("address")},{TAGS::T_ADDRESS}},
+        {{makeRegex("applet")},{TAGS::T_APPLET},{{ATTRIBUTES::A_ALIGN},{ATTRIBUTES::A_ALT},{ATTRIBUTES::A_ARCHIVE},{ATTRIBUTES::A_CODE},{ATTRIBUTES::A_CODEBASE},{ATTRIBUTES::A_DATAFLD},{ATTRIBUTES::A_DATASRC},{ATTRIBUTES::A_HEIGHT},{ATTRIBUTES::A_HSPACE},{ATTRIBUTES::A_MAYSCRIPT},{ATTRIBUTES::A_NAME},{ATTRIBUTES::A_OBJECT},{ATTRIBUTES::A_SRC},{ATTRIBUTES::A_VSPACE},{ATTRIBUTES::A_WIDTH}}}, /// obsolete
+        {{makeEmptyTagRegex("area")},{TAGS::T_AREA},{{ATTRIBUTES::A_ALT},{ATTRIBUTES::A_COORDS},{ATTRIBUTES::A_HREF},{ATTRIBUTES::A_HREFLANG},{ATTRIBUTES::A_PING},{ATTRIBUTES::A_REFERRERPOLICY},{ATTRIBUTES::A_REL},{ATTRIBUTES::A_SHAPE},{ATTRIBUTES::A_TARGET},{ATTRIBUTES::A_ACCESSKEY},{ATTRIBUTES::A_NAME},{ATTRIBUTES::A_NOHREF},{ATTRIBUTES::A_TABINDEX},{ATTRIBUTES::A_TYPE}}}, /// empty
+        {{makeRegex("article")},{TAGS::T_ARTICLE}},
+        {{makeRegex("aside")},{TAGS::T_ASIDE}},
         {{makeRegex("audio")},{TAGS::T_AUDIO},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("b")},{TAGS::T_B},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("base")},{TAGS::T_BASE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("basefont")},{TAGS::T_BASEFONT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("basefont")},{TAGS::T_BASEFONT},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("bdi")},{TAGS::T_BDI},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("bdo")},{TAGS::T_BDO},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("bgsound")},{TAGS::T_BGSOUND},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("big")},{TAGS::T_BIG},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("blink")},{TAGS::T_BLINK},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("bgsound")},{TAGS::T_BGSOUND},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
+        {{makeRegex("big")},{TAGS::T_BIG},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
+        {{makeRegex("blink")},{TAGS::T_BLINK},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("blockquote")},{TAGS::T_BLOCKQUOTE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("body")},{TAGS::T_BODY},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("br")},{TAGS::T_BR},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// empty
         {{makeRegex("button")},{TAGS::T_BUTTON},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("canvas")},{TAGS::T_CANVAS},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("caption")},{TAGS::T_CAPTION},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("center")},{TAGS::T_CENTER},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("center")},{TAGS::T_CENTER},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("cite")},{TAGS::T_CITE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("code")},{TAGS::T_CODE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("col")},{TAGS::T_COL},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("colgroup")},{TAGS::T_COLGROUP},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("command")},{TAGS::T_COMMAND},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("command")},{TAGS::T_COMMAND},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("content")},{TAGS::T_CONTENT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("data")},{TAGS::T_DATA},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("datalist")},{TAGS::T_DATALIST},{{ATTRIBUTES::},{ATTRIBUTES::}}},
@@ -354,19 +362,19 @@ std::unordered_map<const std::regex, const TAGS, const std::vector<ATTRIBUTES> >
         {{makeRegex("details")},{TAGS::T_DETAILS},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("dfn")},{TAGS::T_DFN},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("dialog")},{TAGS::T_DIALOG},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("dir")},{TAGS::T_DIR},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("dir")},{TAGS::T_DIR},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("div")},{TAGS::T_DIV},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("dl")},{TAGS::T_DL},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("dt")},{TAGS::T_DT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("element")},{TAGS::T_ELEMENT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("element")},{TAGS::T_ELEMENT},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("em")},{TAGS::T_EM},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("embed")},{TAGS::T_EMBED},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("fieldset")},{TAGS::T_FIELDSET},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("figcaption")},{TAGS::T_FIGCAPTION},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("figure")},{TAGS::T_FIGURE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("font")},{TAGS::T_FONT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("font")},{TAGS::T_FONT},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("footer")},{TAGS::T_FOOTER},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("form")},{TAGS::T_FORM},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("form")},{TAGS::T_FORM},{{ATTRIBUTES::A_ACCEPT},{ATTRIBUTES::A_ACCEPTCHARSET},{ATTRIBUTES::A_ACTION},{ATTRIBUTES::A_AUTOCAPITALIZE},{ATTRIBUTES::A_AUTOCOMPLETE},{ATTRIBUTES::A_ENCTYPE},{ATTRIBUTES::A_METHOD},{ATTRIBUTES::A_NAME},{ATTRIBUTES::A_NOVALIDATE},{ATTRIBUTES::A_TARGET}}},
         {{makeRegex("frame")},{TAGS::T_FRAME},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("frameset")},{TAGS::T_FRAMESET},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("h1")},{TAGS::T_H1},{{ATTRIBUTES::},{ATTRIBUTES::}}},
@@ -382,9 +390,9 @@ std::unordered_map<const std::regex, const TAGS, const std::vector<ATTRIBUTES> >
         {{makeRegex("html")},{TAGS::T_HTML},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("i")},{TAGS::T_I},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("iframe")},{TAGS::T_IFRAME},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("image")},{TAGS::T_IMAGE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("image")},{TAGS::T_IMAGE},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeEmptyTagRegex("img")},{TAGS::T_IMG},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeEmptyTagRegex("input")},{TAGS::T_INPUT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeEmptyTagRegex("input")},{TAGS::T_INPUT},{{ATTRIBUTES::A_AUTOCOMPLETE},{ATTRIBUTES::A_AUTOFOCUS},{ATTRIBUTES::A_DISABLED},{ATTRIBUTES::A_FORM},{ATTRIBUTES::A_LIST},{ATTRIBUTES::A_NAME},{ATTRIBUTES::A_READONLY},{ATTRIBUTES::A_REQUIERED},{ATTRIBUTES::A_TABINDEX},{ATTRIBUTES::A_TYPE},{ATTRIBUTES::A_VALUE}}},
         {{makeRegex("ins")},{TAGS::T_INS},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("isindex")},{TAGS::T_ISINDEX},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("kbd")},{TAGS::T_KBD},{{ATTRIBUTES::},{ATTRIBUTES::}}},
@@ -393,21 +401,21 @@ std::unordered_map<const std::regex, const TAGS, const std::vector<ATTRIBUTES> >
         {{makeRegex("legend")},{TAGS::T_LEGEND},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("li")},{TAGS::T_LI},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("link")},{TAGS::T_LINK},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("listing")},{TAGS::T_LISTING},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("listing")},{TAGS::T_LISTING},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("main")},{TAGS::T_MAIN},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("map")},{TAGS::T_MAP},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("mark")},{TAGS::T_MARK},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("marquee")},{TAGS::T_MARQUEE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("marquee")},{TAGS::T_MARQUEE},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("menu")},{TAGS::T_MENU},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("menuitem")},{TAGS::T_MENUITEM},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("meta")},{TAGS::T_META},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("meter")},{TAGS::T_METER},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("multicol")},{TAGS::T_MULTICOL},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("multicol")},{TAGS::T_MULTICOL},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("nav")},{TAGS::T_NAV},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("nextid")},{TAGS::T_NEXTID},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("nobr")},{TAGS::T_NOBR},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("noembed")},{TAGS::T_NOEMBED},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("noframes")},{TAGS::T_NOFRAMES},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("nextid")},{TAGS::T_NEXTID},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
+        {{makeRegex("nobr")},{TAGS::T_NOBR},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
+        {{makeRegex("noembed")},{TAGS::T_NOEMBED},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
+        {{makeRegex("noframes")},{TAGS::T_NOFRAMES},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("noscript")},{TAGS::T_NOSCRIPT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("object")},{TAGS::T_OBJECT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("ol")},{TAGS::T_OL},{{ATTRIBUTES::},{ATTRIBUTES::}}},
@@ -417,7 +425,7 @@ std::unordered_map<const std::regex, const TAGS, const std::vector<ATTRIBUTES> >
         {{makeRegex("p")},{TAGS::T_P},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("param")},{TAGS::T_PARAM},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("picture")},{TAGS::T_PICTURE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("plaintext")},{TAGS::T_PLAINTEXT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("plaintext")},{TAGS::T_PLAINTEXT},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("pre")},{TAGS::T_PRE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("progress")},{TAGS::T_PROGRESS},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("q")},{TAGS::T_Q},{{ATTRIBUTES::},{ATTRIBUTES::}}},
@@ -435,9 +443,9 @@ std::unordered_map<const std::regex, const TAGS, const std::vector<ATTRIBUTES> >
         {{makeRegex("slot")},{TAGS::T_SLOT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("small")},{TAGS::T_SMALL},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("source")},{TAGS::T_SOURCE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("spacer")},{TAGS::T_SPACER},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("spacer")},{TAGS::T_SPACER},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("span")},{TAGS::T_SPAN},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("strike")},{TAGS::T_STRIKE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("strike")},{TAGS::T_STRIKE},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("strong")},{TAGS::T_STRONG},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("style")},{TAGS::T_STYLE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("sub")},{TAGS::T_SUB},{{ATTRIBUTES::},{ATTRIBUTES::}}},
@@ -455,11 +463,11 @@ std::unordered_map<const std::regex, const TAGS, const std::vector<ATTRIBUTES> >
         {{makeRegex("title")},{TAGS::T_TITLE},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("tr")},{TAGS::T_TR},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("track")},{TAGS::T_TRACK},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("tt")},{TAGS::T_TT},{{ATTRIBUTES::},{ATTRIBUTES::}}},
+        {{makeRegex("tt")},{TAGS::T_TT},{{ATTRIBUTES::},{ATTRIBUTES::}}}, /// obsolete
         {{makeRegex("u")},{TAGS::T_U},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("ul")},{TAGS::T_UL},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("var")},{TAGS::T_VAR},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeRegex("video")},{TAGS::T_VIDEO},{{ATTRIBUTES::},{ATTRIBUTES::}}},
         {{makeEmptyTagRegex("wbr")},{TAGS::T_WBR},{{ATTRIBUTES::},{ATTRIBUTES::}}},
-        {{makeRegex("xmp")},{TAGS::T_XMP},{{ATTRIBUTES::},{ATTRIBUTES::}}}
+        {{makeRegex("xmp")},{TAGS::T_XMP},{{ATTRIBUTES::},{ATTRIBUTES::}}} /// obsolete
 };
